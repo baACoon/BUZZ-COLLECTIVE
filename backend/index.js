bd035bee463ad498ea6d346b5477e5a80b3ade0c
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-05-04 14:07:13 +0800

    COMMIT 2
    index js and json

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 import express from "express"
+import mysql from "mysql"
+
 
 
 const app = express()
+
+const db =  mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: ""
+})
+
+app.listen(8800, () => {
+    console.log("connected to backend")
+})
