e414d474cde47016bd55abe67f4be124e7404210
gigi <angelaasuntogallo@gmail.com>
2024-05-05 05:12:52 +0800

    Update Login and creating fronten

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from "express"
 import mysql from "mysql"
+import cors from "cors"
 
 
 
 const app = express()
+
+app.use(cors());
+app.use(express.json());
+
 
 const db =  mysql.createConnection ({
     host: "localhost",
@@ -12,6 +17,18 @@ const db =  mysql.createConnection ({
     database: "barbershop"
 })
 
+app.post('/Signup', (req, res) => {
+    const sql = "INSERT INTO signup (`name`, `email`, `password`) Values (?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password,
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
 app.listen(8800, () => {
     console.log("connected to backend")
 })
