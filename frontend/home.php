d86ad999fcd7a52539e489052d5197b158282232
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-05-13 15:18:06 +0800

    frontend
    1st commit

---
+++ frontend/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Home</title>
+</head>
+<body>
+
+        <header id="mainheader">
+            <img src="image/buzz.png" alt="">
+
+            <nav id="navbar">
+                <div class="buttons">
+                    <a href="">Home</a>
+                    <a href="">About us</a>
+                    <a href="">Services</a>
+                    <a href="">Products</a>
+                    <a class="usericon" href=""><i class="fa-regular fa-user"></i></a>
+                    
+                </div>
+     
+            </nav>
+
+        </header>
+    
+</body>
+</html>
