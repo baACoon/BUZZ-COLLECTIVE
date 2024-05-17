57f7442ae6e9e7f999087923635a0fcfa6b44150
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-05-18 01:50:09 +0800

    update
    backend, about us & login/register

--- frontend/aboutus.php
+++ frontend/aboutus.php
@@ -1,33 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="aboutus.css">
+
+    <link rel="stylesheet" href="about.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <title>About us</title>
+
 </head>
 <body>
 
         <header id="mainheader">
        
                 <img src="image/buzz.png" alt="">
+                <div class="buttons">
                     <a href="home.php">HOME </a>
                     <div class="dropdown">
-                        <a class="dropbtn" href= "aboutus.php">ABOUT US <i class="fa-solid fa-chevron-down"></i> </a>
+                        <a class="dropbtn" href="aboutus.php">ABOUT US <i class="fa-solid fa-chevron-down"></i> </a>
                         <div class="dropdown-content">
-                            <li href="aboutus.php">About us</li>
-                            <li href="#">Barbers</li>
+                            <li><a href="aboutus.php">About us</a></li>
+                            <li><a href="#">Barbers</a></li>
                         </div>
                     </div>
                     <a href="#">SERVICES</a>
                     <a href="#">PRODUCTS </a>
                     <a class="usericon" href=""><i class="fa-solid fa-user"></i></a>
+
+                </div>
         </header>
 
 
-        <button class="book-appointment-btn">BOOK AN APPOINTMENT</button>
+        <div id="body-container">
+
+            <img src="image/buzz.png" class="logo-body"alt="">
+            <p>Buzz & Collectives is more than just a barbershop—it's a dynamic concept store 
+                where craftsmanship meets community. Experience top-notch grooming services 
+                alongside curated selections of lifestyle products, fostering a vibrant space 
+                where style and culture intersect.</p>
+
+
+        </div>
+
+        <a href="appointment.php"><button class="book-appointment-btn">BOOK AN APPOINTMENT</button></a>
+
+
     
 </body>
 </html>
--- frontend/login.php
+++ frontend/login.php
@@ -1,17 +1,33 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/BUZZ-COLLECTIVE/backend/server.php'); ?>  
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Login</title>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="login_register.css">
 </head>
 <body>
-    <h1>hello world
-        isa pa test lang 
-    </h1>
-
-    <?php
-
-
-    ?>
-    
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
 </body>
 </html>
