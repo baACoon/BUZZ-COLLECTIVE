f32b5c01cd6dd76eb97d8563595b24393ebfc05f
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-06-26 19:05:47 +0800

    update
    mema lang

--- frontend/appointment.php
+++ frontend/appointment.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <title>Appointment Form</title>
+
 </head>
 <body>
     <div id="buzz-img">
