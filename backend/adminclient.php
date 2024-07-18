d9ed855b1767430333c59a85ba2a78f6bc89cd8d
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-07-18 08:48:32 +0800

    update

---
+++ backend/adminclient.php
@@ -0,0 +1,19 @@
+<?php 
+//connection to database 
+
+session_start();
+
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'barbershop');
+
+
+
+//display clients  information
+
+
+?>
---
+++ frontend/admin/clientprofile.php
@@ -0,0 +1,34 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Client Profile</title>
+</head>
+<body>
+                <aside class="sidebar">
+                    <div class="logo">
+                        <img src="images/BUZZ-White.png" alt="Buzz Collective Logo">
+                    </div>
+                        <nav>
+                            <ul>
+                            <li><a href="admin-appointment.php">Appointment Bookings</a></li>
+                            <li><a href="admin-barber.php">Barbers' Schedule</a></li>
+                            <li><a href="services.php">Services</a><span class="notification-dot"></span></li>
+                            <li><a href="admin-aboutus.php">About Us</a></li>
+                            <li><a href="news.php">News</a></li>
+                            <li><a href="branches.php">Branches</a></li>
+                            <li><a href="clientprofile.php">Client Profile</a></li>
+                            <li><a href="settings.php">Settings</a></li>
+                            </ul>
+                        </nav>
+                </aside>
+ 
+
+
+</body>
+</html>
