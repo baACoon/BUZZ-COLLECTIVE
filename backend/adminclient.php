7cd308b3403b59735a9ccf7330006341e098ca4e
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-08-06 02:54:31 +0800

    Update adminclient.php

--- backend/adminclient.php
+++ backend/adminclient.php
@@ -1,45 +1,41 @@
 <?php 
-//connection to database 
+// Connection to database 
 session_start();
 
-// initializing variables
+// Initializing variables
 $username = "";
 $email    = "";
 $errors = array(); 
 
-// connect to the database
+// Connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'barbershop');
 
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-
-//display clients  information
+// Display clients' information
 $sql = "SELECT * FROM users";
-$result = $db ->query($sql);
+$result = $db->query($sql);
 
-if ($result - $num_rows > 0 ) {
-    while($row = $result -> fetch_assoc()) {
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
         echo "<tr>
                 <td><input type='checkbox' name='select'></td>
-                <td>{$row['appointment_id']}<br></td>
+                <td>{$row['id']}<br></td>
                 <td>
-                    <strong>First Name:</strong> {$row['first_name']}<br>
-                    <strong>Last Name:</strong> {$row['last_name']}<br>
-                    <strong>Email:</strong> {$row['email']}<br>
-                    <strong>Phone Number:</strong> {$row['phone_num']}
+                     {$row['username']}<br>
                 </td>
                 <td>
-                    <strong>Service:</strong> {$row['services']}<br>
-                    <strong>Stylist:</strong> {$row['barber']}
+                     {$row['email']}<br>
                 </td>
                 <td>
-                    <strong>Date:</strong> {$row['appointment_date']}<br>
-                    <strong>Time:</strong> {$row['appointment_time']}
+                    {$row['password']}<br>
                 </td>
               </tr>";
     }
+} else {
+    echo "<tr><td colspan='5'>No records found.</td></tr>";
 }
 
 ?>
