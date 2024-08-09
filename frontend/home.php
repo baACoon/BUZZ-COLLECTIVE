babc3544a9009bcb26722e10c85d0e8738dfceaa
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-08-09 23:01:01 +0800

    update
    kulang hahah

--- frontend/home.php
+++ frontend/home.php
@@ -39,7 +39,7 @@ if (!isset($_SESSION['username'])) {
                     <li><a href="services.php">Services</a></li>
                     <li><a class="usericon" href="#"><i class="fa-solid fa-user"></i></a>
                         <ul class="submenu">
-                            <li><a href="#">My Profile</a></li>
+                            <li><a href="myprofile.php">My Profile</a></li>
                             <li><a href="login.php">Logout</a></li>
                         </ul>
                     </li>
---
+++ frontend/myprofile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="design/profile.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Profile</title>
+    
+</head>
+<body>
+            <aside class="sidebar">
+                    <div class="profile-container">
+                        <img src="default.png" alt="Profile Image" class="profile-img" id="profile-img">
+                        <input type="file" id="profile-image-input">
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="myprofile.php">My Profile</a></li>
+                            <li><a href="#">See History</a></li>
+                            <li><a href="#">Change Password</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </nav>
+            </aside>
+
+
+</body>
+</html>
