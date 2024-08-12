665723d700c8882f8081aa51096505029c6402c2
mverdaluza <122446187+mverdaluza@users.noreply.github.com>
2024-08-13 01:05:33 +0800

    landing page updated (not fully responsive pa)

--- frontend/landingpage.php
+++ frontend/landingpage.php
@@ -1,142 +1,194 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="design/landingpage.css">
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design/landingpage.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <title>Buzz & Collectives Landing Page</title>
 </head>
 <body>
+<nav>
+    <div class="navbar">
+      <i class='bx bx-menu'></i>
+      <div class="logo">
+		<a href="landingpage.php"><img src="design/image/BUZZ-Black.png"></a>
+	  </div>
+      <div class="nav-links">
+        <div class="sidebar-logo">
+          <img src="design/image/BUZZ-Black.png">
+          <i class='bx bx-x' ></i>
+        </div>
+        <ul class="links">
+          <li><a href="#">HOME</a></li>
+          <li>
+            <a href="#">ABOUT US</a>
+            <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
+            <ul class="htmlCss-sub-menu sub-menu">
+              <li><a href="aboutus.php">Buzz & Collectives</a></li>
+              <li><a href="aboutushiring.php">Be a Buzzing Barber</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="branches.php">BRANCHES</a>
+          </li>
+          <li><a href="services.php">SERVICES</a></li>
+			<li><button class="login"><a href="login.php">Sign In</a></button></li>
+        </ul>
+      </div>
 
-<header id="mainheader">
-            <div class="logo">
-                <a href="#"><img src="design/image/BUZZ-Black.png" alt="Logo"></a>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="register.php">Home</a></li>
-                    <li><a href="aboutusland.php">About Us</a>
-                        <ul class="submenu">
-                            <li><a href="aboutus.php">Buzz & Collectives</a></li>
-                            <li><a href="aboutushiring.php">Be a Buzzing Barber</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="branches.php">Branches</a></li>
-                    <li><a href="services.php">Services</a></li>
-                </ul>
-                    <li><button class="login"><a href="login.php">Sign In</a></button></li>
-            </nav>
-        </header>
+    </div>
+  </nav>
+  
+	 
+
+  
     <div id="landing-container">
-        <h1>MAKE YOURSELF FEEL GOOD WITH QUALITY HAIRCUTS</h1>
-        <p>When it comes to haircuts, Buzz & Collectives got you covered!</p>
-        <button class="landing-button"><a href="register.php">Book with us!</a></button>
-        <a href="#" class="see-more">See More</a>
+    <h1>MAKE YOURSELF FEEL GOOD WITH QUALITY HAIRCUTS</h1>
+    <p>When it comes to haircuts, Buzz & Collectives got you covered!</p>  
+    <div class="landing-button-container">
+      <button class="landing-button"><a href="register.php">Book with us!</a></button>
+      <a href="#" class="see-more">See More</a>
     </div>
-
-    <div class="landing-images">
-        <img src="design/image/haircut10.jpg" alt="">
-        <img src="design/image/haircut2.jpg" alt="">
-        <img src="design/image/haircut13.jpg" alt="">
+  </div>
+  
+  
+  <div class="landing-images">
+      <img src="design/image/haircut10.jpg" alt="">
+      <img src="design/image/haircut2.jpg" alt="">
+      <img src="design/image/haircut13.jpg" alt="">
     </div>
-
-    <div class="landing-txt-images">
-        <h1>MODERN AND TRENDING HAIRCUTS</h1>
-        <p>Where barbers at Buzz & Collectives are skilled and has a lot to offer!</p>
-        <img src="design/image/haircut9.jpg" alt="">
+	
+	<div class="landing-txt-images">
+      <h1>MODERN AND TRENDING HAIRCUTS</h1>
+      <p>Where barbers at Buzz & Collectives are skilled and has a lot to offer!</p>
+      <img src="design/image/haircut9.jpg" alt="">
     </div>
 
     <div class="landing-images2">
-        <img src="design/image/haircut5.jpg" alt="">
-        <img src="design/image/haircut6.jpg" alt="">
-        <img src="design/image/haircut4.jpg" alt="">
+      <img src="design/image/haircut5.jpg" alt="">
+      <img src="design/image/haircut6.jpg" alt="">
+      <img src="design/image/haircut4.jpg" alt="">
     </div>
 
     <div class="landing-txt-images2">
-        <h1>HAIRSTYLE AND LIFESTYLE PRODUCTS</h1>
-        <p>Buzz & Collectives is more than just a barbershop—it's a dynamic concept store where craftmanship meets community.</p>
-        <img src="design/image/haircut8.jpg" alt="">
+      <h1>HAIRSTYLE AND LIFESTYLE PRODUCTS</h1>
+      <p>Buzz & Collectives is more than just a barbershop—it's a dynamic concept store where craftmanship meets community.</p>
+      <img src="design/image/haircut8.jpg" alt="">
     </div>
 
     <div class="landing-images3">
-        <img src="design/image/product1.jpg" alt="">
-        <img src="design/image/product3.jpg" alt="">
-        <img src="design/image/product2.jpg" alt="">
+      <img src="design/image/product1.jpg" alt="">
+      <img src="design/image/product3.jpg" alt="">
+      <img src="design/image/product2.jpg" alt="">
     </div>
-    
+
     <a href="#" class="and-more">and more!</a>
 
     <hr>
-
-    <div class="salitran">
-        <div class="branch">
-            <p class="experience">EXPERIENCE MORE AS BUZZ & COLLECTIVES HAPPILY PRESENTS THAT</p>
-            <h1>SALITRAN BRANCH</h1>
-            <p class="is-now-open">IS NOW OPEN!</p>
-            <div class="row-images">
-                <img src="design/image/salitran.jpg" alt="">
-                <img src="design/image/product2.jpg" alt="">
-                <img src="design/image/salitran2.jpg" alt="">
-                <img src="design/image/salitran4.jpg" alt="">
-                <img src="design/image/salitran5.jpg" alt="">
-            </div>
-          
-            <p>Located at</p>
-            <p class="salitran-add">RVV-88 Commercial Center Jose Abad Santos Ave., Salitran 2, Dasmarinas City, Cavite</p>
-            <hr>
-        </div>
+	
+	
+	<div class="salitran">
+      <div class="branch">
+          <p class="experience">EXPERIENCE MORE AS BUZZ & COLLECTIVES HAPPILY PRESENTS THAT</p>
+          <h1>SALITRAN BRANCH</h1>
+          <p class="is-now-open">IS NOW OPEN!</p>
+      </div>
     </div>
+
+    <section class="main swiper mySwiper">
+      <div class="wrapper swiper-wrapper">
+        <div class="slide swiper-slide">
+          <img src="design/image/salitran.jpg" alt="" class="image" />
+        </div>
+        
+      <div class="slide swiper-slide">
+        <img src="design/image/product2.jpg" alt="" class="image" />
+      </div>
+
+      <div class="slide swiper-slide">
+        <img src="design/image/salitran2.jpg" alt="" class="image" />
+      </div>
+      
+      <div class="slide swiper-slide">
+        <img src="design/image/salitran4.jpg" alt="" class="image" />
+      </div>
+      
+      <div class="slide swiper-slide">
+        <img src="design/image/salitran5.jpg" alt="" class="image" />
+      </div>
+    </div>
+
+      <div class="swiper-button-next nav-btn"></div>
+      <div class="swiper-button-prev nav-btn"></div>
+      <div class="swiper-pagination"></div>
+    </section>
+
+    <div class="location">
+      <p>Located at</p>
+      <p class="salitran-add">RVV-88 Commercial Center Jose Abad Santos Ave., Salitran 2, Dasmarinas City, Cavite</p>
+    </div>
+
+    <hr>
 
     <div class="buzz-and-collectives">
-        <p>EXPERIENCE GREATNESS WITH</p>
-        <h1>BUZZ&COLLECTIVES</h1>
-        
-        <div class="container">
-           <button class="book"><a href="register.php">Book with us!</a></button>
+      <p>EXPERIENCE GREATNESS WITH</p>
+      <h1>BUZZ & COLLECTIVES</h1>  
+      <div class="container">
+        <button class="book"><a href="register.php">Book with us!</a></button>
+      </div>
+    </div> 
+	
+	
+	 <footer>
+      <div class="footer-content">
+        <div class="footer-left">
+          <a href="aboutus.php"><img src="design/image/buzz.png" class="footer-logo" alt=""></a>
         </div>
 
-    </div>
-
-
-    <footer>
-        <div class="footer-content">
-            <div class="footer-left">
-                <a href="aboutus.php"><img src="design/image/buzz.png" class="footer-logo" alt=""></a>
-            </div>
-
-            <div class="footer-left1">
-                <h3>SIGN UP FOR NEWS AND PROMOS</h3>
-                    <form>
-                        <input type="email" placeholder="buzzandcollective@gmail.com">  
-                        <button type="submit"><i class="fa-solid fa-arrow-right" style="color: #ffffff;"></i></button>
-                    </form>
-                    <address>
-                        <p><i class="fa-solid fa-location-dot" style="color: #ffffff;"></i> <a href="https://www.google.com/maps/place/Buzz+%26+Collective/@14.4412671,120.9296644,13z/data=!4m20!1m13!4m12!1m4!2m2!1d121.0417152!2d14.49984!4e1!1m6!1m2!1s0x3397d3fd93f7bdc5:0x776c208c818a1b77!2sbuzz+and+collective!2m2!1d120.9390436!2d14.4256597!3m5!1s0x3397d3fd93f7bdc5:0x776c208c818a1b77!8m2!3d14.4256826!4d120.9390514!16s%2Fg%2F11qb5sx_vm?entry=ttu"> 89 Nueno Ave, Imus Cavite, Imus, Philippines, 4103</a></p>
-                        <p><i class="fa-solid fa-envelope" style="color: #ffffff;"></i> buzzandcollective@gmail.com</p>
-                        <p><i class="fa-solid fa-phone" style="color: #ffffff;"></i> 0995 451 5631</p>
-                    </address>
-            </div>
-
-            <div class="footer-right">
-                <h3>BUZZ & COLLECTIVES</h3>
-                <ul>
-                    <li><a href="aboutus.php">About Us</a></li>
-                    <li><a href="aboutushiring.php">Be a Buzzing Barber</a></li>
-                    <li><a href="branches.php">Branches</a></li>
-                    <li><a href="services.php">Services</a></li>
-                </ul>
-            </div>
-
-
-            <div class="social-media">
-                <a href="https://www.instagram.com/buzzncollective?igsh=NTk4eTR5dHBzMThi"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a>
-                <a href="https://www.facebook.com/buzzncollective"><i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i></a>
-                <a href="https://www.tiktok.com/@buzzncollective?_t=8mhQewDUUCI&_r=1"><i class="fa-brands fa-tiktok" style="color: #ffffff;"></i></a>
-            </div>
+        <div class="footer-left1">
+          <h3>SIGN UP FOR NEWS AND PROMOS</h3>
+          <form>
+            <input type="email" placeholder="buzzandcollective@gmail.com">  
+            <button type="submit"><i class="fa-solid fa-arrow-right" style="color: #ffffff;"></i></button>
+          </form>
+          <address>
+              <p><i class="fa-solid fa-location-dot" style="color: #ffffff;"></i> <a href="https://www.google.com/maps/place/Buzz+%26+Collective/@14.4412671,120.9296644,13z/data=!4m20!1m13!4m12!1m4!2m2!1d121.0417152!2d14.49984!4e1!1m6!1m2!1s0x3397d3fd93f7bdc5:0x776c208c818a1b77!2sbuzz+and+collective!2m2!1d120.9390436!2d14.4256597!3m5!1s0x3397d3fd93f7bdc5:0x776c208c818a1b77!8m2!3d14.4256826!4d120.9390514!16s%2Fg%2F11qb5sx_vm?entry=ttu"> 89 Nueno Ave, Imus Cavite, Imus, Philippines, 4103</a></p>
+              <p><i class="fa-solid fa-envelope" style="color: #ffffff;"></i> buzzandcollective@gmail.com</p>
+              <p><i class="fa-solid fa-phone" style="color: #ffffff;"></i> 0995 451 5631</p>
+          </address>
         </div>
+
+        <div class="footer-right">
+          <h3>BUZZ & COLLECTIVES</h3>
+            <ul>
+              <li><a href="aboutus.php">About Us</a></li>
+              <li><a href="aboutushiring.php">Be a Buzzing Barber</a></li>
+              <li><a href="branches.php">Branches</a></li>
+              <li><a href="services.php">Services</a></li>
+            </ul>
+        </div>
+
+        <div class="social-media">
+          <a href="https://www.instagram.com/buzzncollective?igsh=NTk4eTR5dHBzMThi"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a>
+          <a href="https://www.tiktok.com/@buzzncollective?_t=8mhQewDUUCI&_r=1"><i class="fa-brands fa-tiktok" style="color: #ffffff;"></i></a>
+          <a href="https://www.facebook.com/buzzncollective"><i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i></a>
+        </div>
+      </div>
+
+     <!-- <div class="footer-copyright">
+        <p>&#169; 2024, <a href="landingpage.php">Buzz & Collectives</a>.</p>
+      </div> -->
+
     </footer>
 
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="landingscript.js"></script>
 </body>
 </html>
---
+++ frontend/landingscript.js
@@ -0,0 +1,40 @@
+// Image Slider
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+
+menuOpenBtn.onclick = function() {
+  navLinks.classList.add("show");
+};
+
+menuCloseBtn.onclick = function() {
+  navLinks.classList.remove("show");
+};
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
