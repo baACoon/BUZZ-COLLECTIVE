a290a340aff7a5d0cc2e4e958079b7c04aa8b0bc
gigi <angelaasuntogallo@gmail.com>
2024-10-14 01:43:58 +0800

    body email adjustment

--- backend/confirm_appointment.php
+++ backend/confirm_appointment.php
@@ -24,52 +24,65 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
     
             // Fetch appointment details for sending an email (if required)
-            $sql = "SELECT email, first_name, date, timeslot FROM appointments WHERE appointment_id = ?";
+            $sql = "SELECT email, first_name, date, timeslot, services, barber FROM appointments WHERE appointment_id = ?";
             $stmt = $db->prepare($sql);
-            $stmt->bind_param('i',$appointments); // Use the last appointment ID
-            $stmt->execute();
-            $stmt->store_result();
-    
-            // Check if the appointment exists
-            if ($stmt->num_rows > 0) {
-                $stmt->bind_result($email, $first_name, $date, $timeslot, $barber, $services, $barber);
-                $stmt->fetch();
-    
-                // Prepare email notification using PHPMailer
-                require __DIR__ . '/../vendor/phpmailer/phpmailer/src/Exception.php';  
-                require __DIR__ . '/../vendor/phpmailer/phpmailer/src/PHPMailer.php'; 
-                require __DIR__ . '/../vendor/phpmailer/phpmailer/src/SMTP.php';   
-    
-                $mail = new PHPMailer(true);
-                try {
-                    $mail->isSMTP();
-                    $mail->Host       = 'smtp.gmail.com';                                                                                                                                                                    // Set the SMTP server to send through
-                    $mail->SMTPAuth   = true;
-                    $mail->Username   = 'buzzincollective@gmail.com';                                                                                                                                                       // SMTP username
-                    $mail->Password   = 'nwpeckxgsmpbimlb';                                                                                                                                                                 // SMTP password
-                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-                    $mail->Port       = 587;
- 
-    
-                    $mail->setFrom('no-reply@buzzcollective.com', 'Buzz & Collective');
-                    $mail->addAddress($email); // Add the customer's email
-    
-                    // Email content
-                    $mail->isHTML(true);
-                    $mail->Subject = 'Your Appointment is Confirmed!';
-                    $mail->Body    = "Dear $first_name,<br><br>We are pleased to confirm your appointment with us. Below are the details of your appointment:\n <strong>Appointment Details:</strong>Date: $date<br>Time: $timeslot<br>Service: $services<br>Barber: $barber;<br><br>See you!<br>Buzz & Collective";
             
-                    $mail->send();
-                    echo json_encode(['status' => 'success', 'message' => 'Appointment confirmed and email sent successfully.']);
-                } catch (Exception $e) {
-                    echo json_encode(['status' => 'success', 'message' => 'Appointment confirmed but email failed: ' . $mail->ErrorInfo]);
+            foreach ($appointments as $appointmentId) {
+                $stmt->bind_param('i', $appointmentId); // Bind each appointment ID
+                $stmt->execute();
+                $stmt->store_result();
+            
+                // Check if the appointment exists
+                if ($stmt->num_rows > 0) {
+                    $stmt->bind_result($email, $first_name, $date, $timeslot, $services, $barber);
+                    $stmt->fetch();
+                    
+                    $formattedDate = date("F j, Y", strtotime($date));
+                    $servicesFormatted = str_replace('-', ' ', $services);
+                    $servicesFormatted = ucwords($servicesFormatted); //Capitalize each first letter
+                    // Prepare email notification using PHPMailer
+                    require __DIR__ . '/../vendor/phpmailer/phpmailer/src/Exception.php';  
+                    require __DIR__ . '/../vendor/phpmailer/phpmailer/src/PHPMailer.php'; 
+                    require __DIR__ . '/../vendor/phpmailer/phpmailer/src/SMTP.php';   
+        
+                    $mail = new PHPMailer(true);
+                    try {
+                        $mail->isSMTP();
+                        $mail->Host       = 'smtp.gmail.com';                                                                                                                                                                    // Set the SMTP server to send through
+                        $mail->SMTPAuth   = true;
+                        $mail->Username   = 'buzzincollective@gmail.com';                                                                                                                                                       // SMTP username
+                        $mail->Password   = 'nwpeckxgsmpbimlb';                                                                                                                                                                 // SMTP password
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                        $mail->Port       = 587;
+    
+        
+                        $mail->setFrom('no-reply@buzzcollective.com', 'Buzz & Collective');
+                        $mail->addAddress($email); // Add the customer's email
+        
+                        // Email content
+                        $mail->isHTML(true);
+                        $mail->Subject = 'Your Appointment is Confirmed!';
+                        $mail->Body = "
+                        <p style='color: black;'>Dear $first_name,</p>
+                        <p style='color: black;'>We are pleased to confirm your appointment with us. Below are the details of your appointment:</p>
+                        <strong style='color: black;'>Appointment Details:</strong><br>
+                        <p style='color: black;'>
+                            Date: $formattedDate<br>
+                            Time: $timeslot<br>
+                            Service: $servicesFormatted<br>
+                            Barber: $barber<br>
+                        </p>
+                        <p>See you! <br>Buzz & Collective</p>
+                        ";
+                        $mail->send();
+                        echo json_encode(['status' => 'success', 'message' => 'Appointment confirmed and email sent successfully.']);
+                    } catch (Exception $e) {
+                        echo json_encode(['status' => 'success', 'message' => 'Appointment confirmed but email failed: ' . $mail->ErrorInfo]);
+                    }
+                } else {
+                    echo json_encode(['status' => 'error', 'message' => 'Appointment not found for ID: ' . $appointmentId]);
                 }
-            } else {
-                echo json_encode(['status' => 'error', 'message' => 'Appointment not found.']);
             }
-        } else {
-            echo json_encode(['status' => 'error', 'message' => 'Invalid request method.']);
         }
-
-    }
+}
 ?>
