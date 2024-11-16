f2a6ba286a50c9214cf90e60ecc44725059db710
gigi <angelaasuntogallo@gmail.com>
2024-11-16 14:26:05 +0800

    button update

--- frontend/admin/admin-barber.php
+++ frontend/admin/admin-barber.php
@@ -201,38 +201,5 @@
             }
         </script>
 
-    <style>
-        /* Modal styles */
-        .modal {
-            display: none;
-            position: fixed;
-            z-index: 1;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.5);
-        }
-
-        .modal-content {
-            background-color: #fff;
-            margin: 15% auto;
-            padding: 20px;
-            border: 1px solid #888;
-            width: 300px;
-            text-align: center;
-        }
-
-        #closeModal {
-            background-color: #4CAF50;
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            cursor: pointer;
-        }
-
-    
-    </style>
-
 </body>
 </html>
