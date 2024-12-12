80a5f7edc1584858727adf192c388a41b0d736c6
gigi <angelaasuntogallo@gmail.com>
2024-12-12 23:03:29 +0800

    update

--- frontend/landingpage.php
+++ frontend/landingpage.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="design/image/buzznCollectives.jpg">
-  <link rel="stylesheet" href="design/landingpage.css">
+  <link rel="icon" type="image/x-icon" href="frontend/design/image/buzznCollectives.jpg">
+  <link rel="stylesheet" href="frontend/design/landingpage.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -190,6 +190,6 @@
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="../frontend/js/landingscript.js"></script>
+    <script src="frontend/js/landingscript.js"></script>
 </body>
 </html>
