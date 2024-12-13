9409e1eea257ae4fd9ac56370f67c1bdf8f3112b
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-12-13 23:33:44 +0800

    about us

--- frontend/aboutus.php
+++ frontend/aboutus.php
@@ -289,7 +289,7 @@ $db->close();
     
     
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="landingscript.js"></script>
+    <script src="js/landingscript.js"></script>
     <script src="script.js"></script>
     <script>
       jQuery(document).ready(function ($) {
