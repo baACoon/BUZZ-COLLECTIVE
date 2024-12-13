415157bd551fe055df597d323333cc8d1057613f
gigi <angelaasuntogallo@gmail.com>
2024-12-13 09:42:00 +0800

    landing js update

--- frontend/js/landingscript.js
+++ frontend/js/landingscript.js
@@ -1,5 +1,6 @@
-// Image Slider
-var swiper = new Swiper(".mySwiper", {
+document.addEventListener("DOMContentLoaded", function () {
+  // Image Slider
+  const swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     loop: true,
     pagination: {
@@ -12,29 +13,53 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-// sidebar open close js code
-let navLinks = document.querySelector(".nav-links");
-let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+  // Sidebar open/close js code
+  const navLinks = document.querySelector(".nav-links");
+  const menuOpenBtn = document.querySelector(".navbar .bx-menu");
+  const menuCloseBtn = document.querySelector(".nav-links .bx-x");
 
-menuOpenBtn.onclick = function() {
-  navLinks.classList.add("show");
-};
+  if (menuOpenBtn) {
+    menuOpenBtn.onclick = function () {
+      navLinks?.classList.add("show");
+    };
+  } else {
+    console.error("Menu Open Button not found.");
+  }
 
-menuCloseBtn.onclick = function() {
-  navLinks.classList.remove("show");
-};
+  if (menuCloseBtn) {
+    menuCloseBtn.onclick = function () {
+      navLinks?.classList.remove("show");
+    };
+  } else {
+    console.error("Menu Close Button not found.");
+  }
 
-// sidebar submenu open close js code
-let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-htmlcssArrow.onclick = function() {
- navLinks.classList.toggle("show1");
-}
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function() {
- navLinks.classList.toggle("show2");
-}
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function() {
- navLinks.classList.toggle("show3");
-}
+  // Sidebar submenu open/close js code
+  const htmlcssArrow = document.querySelector(".htmlcss-arrow");
+  const moreArrow = document.querySelector(".more-arrow");
+  const jsArrow = document.querySelector(".js-arrow");
+
+  if (htmlcssArrow) {
+    htmlcssArrow.onclick = function () {
+      navLinks?.classList.toggle("show1");
+    };
+  } else {
+    console.error("HTML/CSS Arrow not found.");
+  }
+
+  if (moreArrow) {
+    moreArrow.onclick = function () {
+      navLinks?.classList.toggle("show2");
+    };
+  } else {
+    console.error("More Arrow not found.");
+  }
+
+  if (jsArrow) {
+    jsArrow.onclick = function () {
+      navLinks?.classList.toggle("show3");
+    };
+  } else {
+    console.error("JS Arrow not found.");
+  }
+});
