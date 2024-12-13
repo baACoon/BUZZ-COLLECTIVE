3b4963251ed65646bf26e70f3ef19018f34c768d
gigi <angelaasuntogallo@gmail.com>
2024-12-13 23:52:55 +0800

    js update

--- frontend/home.php
+++ frontend/home.php
@@ -71,7 +71,7 @@ $db->close();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <base href="https://buzzcollective.gayvar.com/frontend/">
+    <base href="https://buzzcollective.gayvar.com/Buzz-collective/frontend/">
     <title>Buzz & Collective | Home</title>
 </head>
 <body>
