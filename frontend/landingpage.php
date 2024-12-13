4e311071b7416f74ac9e0d39caa5b107127eca54
gigi <angelaasuntogallo@gmail.com>
2024-12-13 09:29:20 +0800

    landing path update

--- frontend/landingpage.php
+++ frontend/landingpage.php
@@ -19,28 +19,28 @@
     <div class="navbar">
       <i class='bx bx-menu'></i>
       <div class="logo">
-		<a href="landingpage.php"><img src="design/image/BUZZ-Black.png"></a>
+		<a href="landingpage.php"><img src="frontend/design/image/BUZZ-Black.png"></a>
 	  </div>
       <div class="nav-links">
         <div class="sidebar-logo">
-          <img src="design/image/BUZZ-Black.png">
+          <img src="frontend/design/image/BUZZ-Black.png">
           <i class='bx bx-x' ></i>
         </div>
         <ul class="links">
-          <li><a href="#">HOME</a></li>
+          <li><a href="frontend/login.php">HOME</a></li>
           <li>
             <a href="#">ABOUT US</a>
             <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
             <ul class="htmlCss-sub-menu sub-menu">
-              <li><a href="aboutus.php">Buzz & Collectives</a></li>
-              <li><a href="aboutushiring.php">Be a Buzzing Barber</a></li>
+              <li><a href="frontend/aboutus.php">Buzz & Collectives</a></li>
+              <li><a href="frontend/aboutushiring.php">Be a Buzzing Barber</a></li>
             </ul>
           </li>
           <li>
             <a href="branches.php">BRANCHES</a>
           </li>
-          <li><a href="services.php">SERVICES</a></li>
-			<li><button class="login"><a href="login.php">Sign In</a></button></li>
+          <li><a href="frontend/services.php">SERVICES</a></li>
+			<li><button class="login"><a href="frontend/login.php">Sign In</a></button></li>
         </ul>
       </div>
 
@@ -54,40 +54,40 @@
     <h1>MAKE YOURSELF FEEL GOOD WITH QUALITY HAIRCUTS</h1>
     <p>When it comes to haircuts, Buzz & Collectives got you covered!</p>  
     <div class="landing-button-container">
-      <button class="landing-button"><a href="register.php">Book with us!</a></button>
+      <button class="landing-button"><a href="frontend/register.php">Book with us!</a></button>
       <a href="#" class="see-more">See More</a>
     </div>
   </div>
   
   
   <div class="landing-images">
-      <img src="design/image/haircut10.jpg" alt="">
-      <img src="design/image/haircut2.jpg" alt="">
-      <img src="design/image/haircut13.jpg" alt="">
+      <img src="frontend/design/image/haircut10.jpg" alt="">
+      <img src="frontend/design/image/haircut2.jpg" alt="">
+      <img src="frontend/design/image/haircut13.jpg" alt="">
     </div>
 	
 	<div class="landing-txt-images">
       <h1>MODERN AND TRENDING HAIRCUTS</h1>
       <p>Where barbers at Buzz & Collectives are skilled and has a lot to offer!</p>
-      <img src="design/image/haircut9.jpg" alt="">
+      <img src="frontend/design/image/haircut9.jpg" alt="">
     </div>
 
     <div class="landing-images2">
-      <img src="design/image/haircut5.jpg" alt="">
-      <img src="design/image/haircut6.jpg" alt="">
-      <img src="design/image/haircut4.jpg" alt="">
+      <img src="frontend/design/image/haircut5.jpg" alt="">
+      <img src="frontend/design/image/haircut6.jpg" alt="">
+      <img src="frontend/design/image/haircut4.jpg" alt="">
     </div>
 
     <div class="landing-txt-images2">
       <h1>HAIRSTYLE AND LIFESTYLE PRODUCTS</h1>
       <p>Buzz & Collectives is more than just a barbershop—it's a dynamic concept store where craftmanship meets community.</p>
-      <img src="design/image/haircut8.jpg" alt="">
+      <img src="frontend/design/image/haircut8.jpg" alt="">
     </div>
 
     <div class="landing-images3">
-      <img src="design/image/product1.jpg" alt="">
-      <img src="design/image/product3.jpg" alt="">
-      <img src="design/image/product2.jpg" alt="">
+      <img src="frontend/design/image/product1.jpg" alt="">
+      <img src="frontend/design/image/product3.jpg" alt="">
+      <img src="frontend/design/image/product2.jpg" alt="">
     </div>
 
     <a href="#" class="and-more">and more!</a>
@@ -106,23 +106,23 @@
     <section class="main swiper mySwiper">
       <div class="wrapper swiper-wrapper">
         <div class="slide swiper-slide">
-          <img src="design/image/salitran.jpg" alt="" class="image" />
+          <img src="frontend/design/image/salitran.jpg" alt="" class="image" />
         </div>
         
       <div class="slide swiper-slide">
-        <img src="design/image/product2.jpg" alt="" class="image" />
+        <img src="frontend/design/image/product2.jpg" alt="" class="image" />
       </div>
 
       <div class="slide swiper-slide">
-        <img src="design/image/salitran2.jpg" alt="" class="image" />
+        <img src="frontend/design/image/salitran2.jpg" alt="" class="image" />
       </div>
       
       <div class="slide swiper-slide">
-        <img src="design/image/salitran4.jpg" alt="" class="image" />
+        <img src="frontend/design/image/salitran4.jpg" alt="" class="image" />
       </div>
       
       <div class="slide swiper-slide">
-        <img src="design/image/salitran5.jpg" alt="" class="image" />
+        <img src="frontend/design/image/salitran5.jpg" alt="" class="image" />
       </div>
     </div>
 
@@ -142,7 +142,7 @@
       <p>EXPERIENCE GREATNESS WITH</p>
       <h1>BUZZ & COLLECTIVES</h1>  
       <div class="container">
-        <button class="book"><a href="register.php">Book with us!</a></button>
+        <button class="book"><a href="frontend/register.php">Book with us!</a></button>
       </div>
     </div> 
 	
@@ -150,7 +150,7 @@
 	 <footer>
       <div class="footer-content">
         <div class="footer-left">
-          <a href="aboutus.php"><img src="design/image/buzz.png" class="footer-logo" alt=""></a>
+          <a href="frontend/aboutus.php"><img src="frontend/design/image/buzz.png" class="footer-logo" alt=""></a>
         </div>
 
         <div class="footer-left1">
@@ -169,10 +169,10 @@
         <div class="footer-right">
           <h3>BUZZ & COLLECTIVES</h3>
             <ul>
-              <li><a href="aboutus.php">About Us</a></li>
-              <li><a href="aboutushiring.php">Be a Buzzing Barber</a></li>
-              <li><a href="branches.php">Branches</a></li>
-              <li><a href="services.php">Services</a></li>
+              <li><a href="frontend/aboutus.php">About Us</a></li>
+              <li><a href="frontend/aboutushiring.php">Be a Buzzing Barber</a></li>
+              <li><a href="frontend/branches.php">Branches</a></li>
+              <li><a href="frontend/services.php">Services</a></li>
             </ul>
         </div>
 
