1f48a118ae3461f989c31609e7e21327f98cfb9d
gigi <angelaasuntogallo@gmail.com>
2024-12-13 10:41:21 +0800

    login update

--- frontend/login.php
+++ frontend/login.php
@@ -1,35 +1,48 @@
-<!--<?php //include($_SERVER['DOCUMENT_ROOT'] . '/BUZZ-COLLECTIVE/backend/server.php'); ?> -->
-<?php include('../backend/server.php')?>
-
+<?php 
+// Include the server logic file
+include('../backend/server.php'); 
+?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Buzz & Collective - log in</title>
-  <link rel="icon" type="image/x-icon" href="frontend/design/image/buzznCollectives.jpg">
-  <link rel="stylesheet" type="text/css" href="frontend/design/login.css?v=51">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="design/image/buzznCollectives.jpg">
+  <link rel="stylesheet" type="text/css" href="design/login.css?v=51">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <title>Buzz & Collective - Login</title>
 </head>
 <body>
-    <div class="container">
-        <div class="login-form">
-            <h2>LOGIN</h2>
-            <form method="post" action="frontend/login.php">
-			<?php include('errors.php');?>
-                <h4>USERNAME</h4>
-                <input type="text" name="username" placeholder="Username" required style="font-family:'Montserrat', sans-serif;">
-                <h4>PASSWORD</h4>
-                <input type="password" name="password" placeholder="Password" required style="font-family:'Montserrat', sans-serif;">
-                <input type="submit" value="Login" name="login_user" style="font-family:'Montserrat', sans-serif;">
-            </form>
-            <h3>Don't have account? <a href="frontend/register.php">SIGN UP</a>
-            <h3>Forgot password?</h3>
-        </div>
-        <div class="image-section">
-            <!-- Image and text overlay -->
-        </div>
+  <div class="container">
+    <div class="login-form">
+      <h2>LOGIN</h2>
+      <!-- Login form -->
+      <form method="post" action="../backend/server.php">
+        <?php 
+          // Include any error messages if present
+          if (file_exists('errors.php')) {
+            include('errors.php'); 
+          }
+        ?>
+        <h4>USERNAME</h4>
+        <input type="text" name="username" placeholder="Username" required style="font-family:'Montserrat', sans-serif;">
+        
+        <h4>PASSWORD</h4>
+        <input type="password" name="password" placeholder="Password" required style="font-family:'Montserrat', sans-serif;">
+        
+        <input type="submit" value="Login" name="login_user" style="font-family:'Montserrat', sans-serif;">
+      </form>
+
+      <h3>Don't have an account? <a href="register.php">SIGN UP</a></h3>
+      <h3>Forgot password?</h3>
     </div>
+    <div class="image-section">
+      <!-- Add an image or content here if needed -->
+    </div>
+  </div>
 </body>
 </html>
