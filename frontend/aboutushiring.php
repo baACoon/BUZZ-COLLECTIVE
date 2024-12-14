d9752dd74dfa9a9eeb6651d67ea3bc796f43a66c
maiyv <marielle.verdaluza07@gmail.com>
2024-12-14 19:44:05 +0800

    update js

--- frontend/aboutushiring.php
+++ frontend/aboutushiring.php
@@ -178,7 +178,25 @@ $db->close();
             </div>
         </footer>  
     
-        <script src="../frontend/js/aboutus.js"></script>
+        <script>
+            // sidebar open close js code
+            let navLinks = document.querySelector(".nav-links");
+            let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+            let menuCloseBtn = document.querySelector(".nav-links .bx-x");
 
+            menuOpenBtn.onclick = function() {
+            navLinks.classList.add("show");
+            };
+
+            menuCloseBtn.onclick = function() {
+            navLinks.classList.remove("show");
+            };
+
+            // sidebar submenu open close js code
+            let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+            htmlcssArrow.onclick = function() {
+            navLinks.classList.toggle("show1");
+            }
+        </script>
 </body>
 </html>
--- frontend/services.php
+++ frontend/services.php
@@ -213,6 +213,40 @@ $services = json_decode(file_get_contents('../frontend/admin/data/services.json'
             </div>
         </div>
     </footer>
-    <script src="../frontend/js/services.js"></script>
+    <script>
+        // sidebar open close js code
+        let navLinks = document.querySelector(".nav-links");
+        let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+        let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+
+        menuOpenBtn.onclick = function() {
+        navLinks.classList.add("show");
+        };
+
+        menuCloseBtn.onclick = function() {
+        navLinks.classList.remove("show");
+        };
+
+        // sidebar submenu open close js code
+        let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+        htmlcssArrow.onclick = function() {
+        navLinks.classList.toggle("show1");
+        }
+
+
+        function showMoreImages() {
+            var additionalImages = document.querySelector('.additional-images');
+            var seeMoreLink = document.querySelector('.see-more-link');
+            
+            // Toggle visibility of additional images
+            if (additionalImages.style.display === 'none' || additionalImages.style.display === '') {
+                additionalImages.style.display = 'grid';  // Show the images
+                seeMoreLink.textContent = 'See Less'; // Change text to 'See Less'
+            } else {
+                additionalImages.style.display = 'none'; // Hide the images
+                seeMoreLink.textContent = 'See More'; // Change text back to 'See More'
+            }
+        }
+    </script>
 </body>
 </html>
