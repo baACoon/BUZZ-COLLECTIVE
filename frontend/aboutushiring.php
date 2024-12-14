80425c62d6e15fcf3ef832154e584b5c73ec932d
maiyv <marielle.verdaluza07@gmail.com>
2024-12-14 19:36:24 +0800

    try

--- frontend/aboutushiring.php
+++ frontend/aboutushiring.php
@@ -178,8 +178,7 @@ $db->close();
             </div>
         </footer>  
     
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="landingscript.js"></script>
+        <script src="../frontend/js/aboutus.js"></script>
 
 </body>
 </html>
