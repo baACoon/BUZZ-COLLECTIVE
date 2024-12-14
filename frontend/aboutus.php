b72f20607294e5244a7997347a69f1ba545271d3
maiyv <marielle.verdaluza07@gmail.com>
2024-12-14 19:04:07 +0800

    about us update

--- frontend/aboutus.php
+++ frontend/aboutus.php
@@ -169,86 +169,6 @@ $db->close();
         <button class="barbers-button"><a href="appointment.php">BOOK AN APPOINTMENT</a></button>
     </section>
 
-    
-    <script>
-    // Select DOM elements
-    const nextBtn = document.querySelector('.next');
-    const prevBtn = document.querySelector('.prev');
-    const carousel = document.querySelector('.carousel');
-    const list = document.querySelector('.list');
-    const items = document.querySelectorAll('.item');
-
-    let index = 0; // Index for tracking current slider position
-
-    // Timing variables
-    let timeRunning = 3000; 
-    let timeAutoNext = 7000; 
-
-    // Declare a global `runningTime` variable for animation
-    let runningTime = document.createElement('div'); 
-    runningTime.style.animation = 'runningTime 7s linear 1 forwards'; 
-    document.body.appendChild(runningTime); // Temporarily added to the DOM
-
-    // Event listeners for next and previous buttons
-    if (nextBtn) {
-        nextBtn.onclick = function () {
-            showSlider('next');
-        };
-    }
-
-    if (prevBtn) {
-        prevBtn.onclick = function () {
-            showSlider('prev');
-        };
-    }
-
-    let runTimeOut; // Timeout for animation reset
-    let runNextAuto = setTimeout(() => {
-        nextBtn.click(); // Trigger next slider automatically
-    }, timeAutoNext);
-
-    // Function to reset the running time animation
-    function resetTimeAnimation() {
-        if (runningTime) {
-            runningTime.style.animation = 'none'; // Clear the animation
-            runningTime.offsetHeight; // Trigger a reflow
-            runningTime.style.animation = 'runningTime 7s linear 1 forwards'; // Restart the animation
-        }
-    }
-
-    // Function to handle slider transitions
-    function showSlider(type) {
-        let sliderItemsDom = list.querySelectorAll('.carousel .list .item');
-
-        if (type === 'next') {
-            list.appendChild(sliderItemsDom[0]); // Move first item to the end
-            carousel.classList.add('next');
-        } else {
-            list.prepend(sliderItemsDom[sliderItemsDom.length - 1]); // Move last item to the front
-            carousel.classList.add('prev');
-        }
-
-        // Clear the transition timeout
-        clearTimeout(runTimeOut);
-
-        runTimeOut = setTimeout(() => {
-            carousel.classList.remove('next');
-            carousel.classList.remove('prev');
-        }, timeRunning);
-
-        // Clear and reset the auto-next timer
-        clearTimeout(runNextAuto);
-        runNextAuto = setTimeout(() => {
-            nextBtn.click(); // Trigger the next button click
-        }, timeAutoNext);
-
-        resetTimeAnimation(); // Reset the running time animation
-    }
-</script>
-
-
-
-
     <footer>
         <div class="footer-content">
             <div class="footer-left">
@@ -278,7 +198,6 @@ $db->close();
                 </ul>
             </div>
 
-
             <div class="social-media">
                 <a href="https://www.instagram.com/buzzncollective?igsh=NTk4eTR5dHBzMThi"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a>
                 <a href="https://www.facebook.com/buzzncollective"><i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i></a>
@@ -287,18 +206,9 @@ $db->close();
         </div>
     </footer>
     
-    
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="js/landingscript.js"></script>
+    <script src="../frontend/js/aboutus.js"></script>
     <script src="script.js"></script>
-    <script>
-      jQuery(document).ready(function ($) {
-        $(".slider-img").on("click", function () {
-          $(".slider-img").removeClass("active");
-          $(this).addClass("active");
-        });
-      });
-    </script>
 
 </body>
 </html>
