2502d257bdf3c28b6c2be228b8e9db3bfbb6dd00
gigi <angelaasuntogallo@gmail.com>
2024-12-15 10:12:46 +0800

    admin update

--- frontend/admin/admin_log.php
+++ frontend/admin/admin_log.php
@@ -6,11 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - BUZZ Collective</title>
-    <link rel="stylesheet" href="/Designs/admin-logreg.css?v=101">
+    <link rel="stylesheet" href="/Buzz-collective/Designs/admin-logreg.css?v=101">
 </head>
 <body>
     <div id="buzz-img">
-        <img src="/images/BUZZ-White2.png" alt="Buzz Logo">
+        <img src="/Buzz-collective/images/BUZZ-White2.png" alt="Buzz Logo">
     </div>
     <div class="login-container">
         <?php 
@@ -19,14 +19,12 @@
                 <?php foreach ($errors as $error) echo $error . '<br>'; ?>
             </div>
         <?php endif; ?>
-        <form class="login-form" method="post" action="/admin_log.php">
+        <form class="login-form" method="post" action="/Buzz-collective/frontend/admin/admin_log.php">
             <h4>USERNAME</h4>
             <input type="text" placeholder="Username" name="username" required>
             <h4>PASSWORD</h4>
             <input type="password" placeholder="Password" name="password" required>
-            <div style="display: flex; justify-content: space-between; width: 100%; max-width: 300px;">
-                <a href="/admin_reg.php">Sign Up</a>
-            </div>
+            <a href="/Buzz-collective/frontend/admin/admin_reg.php">Sign Up</a>
             <button type="submit" name="log_admin">Login</button>
         </form>
     </div>
--- frontend/admin/admin_reg.php
+++ frontend/admin/admin_reg.php
@@ -6,11 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Registration - BUZZ Collective</title>
-    <link rel="stylesheet" href="/Designs/admin-logreg.css?v=101">
+    <link rel="stylesheet" href="/Buzz-collective/Designs/admin-logreg.css?v=101">
 </head>
 <body>
     <div id="buzz-img">
-        <img src="/images/BUZZ-White2.png" alt="Buzz Logo">
+        <img src="/Buzz-collective/images/BUZZ-White2.png" alt="Buzz Logo">
     </div>
     <div class="login-container2">
         <?php 
@@ -19,17 +19,17 @@
                 <?php foreach ($errors as $error) echo $error . '<br>'; ?>
             </div>
         <?php endif; ?>
-        <form class="login-form2" method="POST" action="/admin_reg.php">
-                <h4>USERNAME</h4>
-                <input type="text" name="username" placeholder="Username" value="<?php echo htmlspecialchars($username); ?>" required>
-                <h4>EMAIL</h4>
-                <input type="email" name="email" placeholder="Email" value="<?php echo htmlspecialchars($email); ?>" required>
-                <h4>PASSWORD</h4>
-                <input type="password" name="password_1" placeholder="Password" required>
-                <h4>CONFIRM PASSWORD</h4>
-                <input type="password" name="password_2" placeholder="Confirm Password" required>
-                <button type="submit" name="reg_admin">Sign Up</button>
-                <a href="/admin_log.php">Login</a>
+        <form class="login-form2" method="POST" action="/Buzz-collective/frontend/admin/admin_reg.php">
+            <h4>USERNAME</h4>
+            <input type="text" name="username" placeholder="Username" value="<?php echo htmlspecialchars($username); ?>" required>
+            <h4>EMAIL</h4>
+            <input type="email" name="email" placeholder="Email" value="<?php echo htmlspecialchars($email); ?>" required>
+            <h4>PASSWORD</h4>
+            <input type="password" name="password_1" placeholder="Password" required>
+            <h4>CONFIRM PASSWORD</h4>
+            <input type="password" name="password_2" placeholder="Confirm Password" required>
+            <button type="submit" name="reg_admin">Sign Up</button>
+            <a href="/Buzz-collective/frontend/admin/admin_log.php">Login</a>
         </form>
     </div>
 </body>
