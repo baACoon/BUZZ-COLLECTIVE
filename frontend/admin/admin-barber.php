ee479653cae5b8127530029eab7bf410d3678d31
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-12-15 20:21:34 +0800

    TEST 12

--- frontend/admin/admin-barber.php
+++ frontend/admin/admin-barber.php
@@ -111,11 +111,11 @@ if (isset($_GET['error'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="../design/image/buzznCollectives.jpg">
-    <base href="https://admin.buzzcollective.gayvar.com/Buzz-collective/frontend/admin/">
+    <link rel="icon" type="image/x-icon" href="/image/buzznCollectives.jpg">
     <link rel="stylesheet" href="/Designs/adminbarber.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <base href="https://admin.buzzcollective.gayvar.com/Buzz-collective/frontend/admin/">
     <title>Barber Schedule</title>
 </head>
 <body>
