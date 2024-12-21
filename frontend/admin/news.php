eaa8c31297a2e3962eba94b1912c8e16ecbeb89b
maiyv <marielle.verdaluza07@gmail.com>
2024-12-21 16:12:27 +0800

    update

--- frontend/admin/news.php
+++ frontend/admin/news.php
@@ -91,15 +91,15 @@ if ($result) {
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-      <!-- Navbar for screens below 768px -->
-      <div class="mobile-navbar" id="mobile-navbar">
+         <!-- Navbar for screens below 768px -->
+    <div class="mobile-navbar" id="mobile-navbar">
         <div class="mobile-logo">
             <img src="images/BUZZ-Black.png" alt="Buzz Collective Logo">
         </div>
         <i class='bx bx-menu' id="menu-icon"></i>
     </div>
 
-    <aside class="sidebar" id="sidebar">
+    <aside cl-ass="sidebar" id="sidebar">
         <i class='bx bx-x' id="close-sidebar" style="display: none;"></i> <!-- Close button -->
         <div class="logo">
             <a href="../admin/admin-home.php">
