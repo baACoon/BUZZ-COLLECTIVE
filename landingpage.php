bbd472cb5524ab1396a6fab4be2111e05ea51db5
maiyv <marielle.verdaluza07@gmail.com>
2025-01-07 15:23:45 +0800

    Update landingpage.php

--- landingpage.php
+++ landingpage.php
@@ -191,6 +191,26 @@
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="js/landingscript.js"></script>
+    <!--<script src="js/landingscript.js"></script> -->
+    <script>
+      // sidebar open close js code
+      let navLinks = document.querySelector(".nav-links");
+      let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+      let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+
+      menuOpenBtn.onclick = function() {
+        navLinks.classList.add("show");
+      };
+
+      menuCloseBtn.onclick = function() {
+        navLinks.classList.remove("show");
+      };
+
+      // sidebar submenu open close js code
+      let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+        htmlcssArrow.onclick = function() {
+          navLinks.classList.toggle("show1");
+        }
+    </script>
 </body>
 </html>
