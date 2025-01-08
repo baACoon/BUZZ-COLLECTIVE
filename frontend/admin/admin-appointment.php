fc224976d7886bb78f650b2b17cedcfc33c38f3a
gigi <angelaasuntogallo@gmail.com>
2025-01-09 05:28:25 +0800

    adjust path

--- frontend/admin/admin-appointment.php
+++ frontend/admin/admin-appointment.php
@@ -227,7 +227,7 @@ $branches = json_decode($json_data, true);
                     formData.append('appointments[]', appointmentId);
                 });
 
-                fetch('/../../backend/confirm_appointment.php', {
+                fetch('../../backend/confirm_appointment.php', {
                     method: 'POST',
                     body: formData
                 })
@@ -259,7 +259,7 @@ $branches = json_decode($json_data, true);
                     formData.append('appointments[]', appointmentId);
                 });
 
-                fetch('../../../../backend/cancel_appointment.php', {
+                fetch('../../backend/cancel_appointment.php', {
                     method: 'POST',
                     body: formData
                 })
