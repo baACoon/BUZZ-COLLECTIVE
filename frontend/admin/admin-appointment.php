d6a51da928c31983854d65558d21b1fd4a27b980
gigi <angelaasuntogallo@gmail.com>
2025-01-08 23:54:29 +0800

    adjust path

--- frontend/admin/admin-appointment.php
+++ frontend/admin/admin-appointment.php
@@ -22,7 +22,7 @@ $branches = json_decode($json_data, true);
     <title>Appointments - Buzz & Collective</title>
     <link rel="icon" type="image/x-icon" href="../design/image/buzznCollectives.jpg">
     <link rel="stylesheet" href="/Designs/adminappointment.css">
-    <base href="https://admin.buzzcollective.gayvar.com/Buzz-collective/frontend/">
+    <base href="https://admin.buzzcollective.gayvar.com/Buzz-collective/frontend/admin/">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
@@ -95,7 +95,7 @@ $branches = json_decode($json_data, true);
                         foreach ($appointments as $appointment) {
         
                             $receipt_link = isset($appointment['receipt']) && !empty($appointment['receipt']) 
-                            ? "<a href='#' class='view-receipt' onclick=\"showReceiptModal('https://admin.buzzcollective.gayvar.com/Buzz-collective/frontend/uploads/receipts/{$appointment['receipt']}')\">View Receipt</a>" 
+                            ? "<a href='#' class='view-receipt' onclick=\"showReceiptModal('./../uploads/receipts/{$appointment['receipt']}')\">View Receipt</a>" 
                             : 'No Receipt Uploaded';
 
                             $branch_name = 'Main Branch';
