9b7c08c53977fba3a2f5a7995f1c496768bf0412
yubanji <99063502+yubanji@users.noreply.github.com>
2025-01-09 02:01:31 +0800

    update

--- frontend/admin/adminprofile.php
+++ frontend/admin/adminprofile.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="Designs/admin-profile.css">
 </head>
 <body>
-    <!-- Navbar for screens below 768px -->
     <div class="mobile-navbar" id="mobile-navbar">
         <div class="mobile-logo">
             <img src="/images/BUZZ-Black.png" alt="Buzz Collective Logo">
@@ -24,24 +23,18 @@
         </div>
         <nav>
             <ul>
-                <li><a href="/adminprofile.php">Admin Profile</a></li>
                 <li><a href="/dashboard.php">Dashboard</a></li>
                 <li><a href="/admin-appointment.php">Appointment Bookings</a></li>
                 <li><a href="/admin-barber.php">Barbers' Schedule</a></li>
-                <li><a href="/services.php">Services</a><span class="notification-dot"></span></li>
-                <li>
-                    <a href="/admin-aboutus.php">About Us</a>
-                    <ul class="htmlCss-sub-menu sub-menu">
-                                <li><a href="/aboutus.php">Barbers</a></li>
-                                <li><a href="/aboutushiring.php">Hiring</a></li>
-                            </ul>
-                </li>
+                <li><a href="/services.php">Services</a></li>
+                <li><a href="/admin-aboutus.php">About Us</a></li>
                 <li><a href="/news.php">News</a></li>
                 <li><a href="/admin-branches.php">Branches</a></li>
+                <li><a href="/settings.php">Settings</a></li>
             </ul>
         </nav>
     </aside>
-    
+
     <div class="profile-container">
         <h1>Admin Profile</h1>
 
