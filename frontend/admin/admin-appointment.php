c43f27cb8d94c1b24511f54283ea69bd764df688
gigi <angelaasuntogallo@gmail.com>
2025-01-09 05:21:38 +0800

    path

--- frontend/admin/admin-appointment.php
+++ frontend/admin/admin-appointment.php
@@ -202,7 +202,7 @@ $branches = json_decode($json_data, true);
             var formData = new FormData(form);
 
             var xhr = new XMLHttpRequest();
-            xhr.open('POST', '../../../../backend/delete_appointment.php', true);
+            xhr.open('POST', '../../backend/delete_appointment.php', true);
             xhr.onreadystatechange = function() {
                 if (xhr.readyState === 4 && xhr.status === 200) {
                     var response = JSON.parse(xhr.responseText);
