13688e2febab551b9007657c7fbbdc780a9688d6
yubanji <99063502+yubanji@users.noreply.github.com>
2025-01-09 01:53:39 +0800

    update style

--- frontend/admin/adminprofile.php
+++ frontend/admin/adminprofile.php
@@ -7,6 +7,41 @@
     <link rel="stylesheet" href="Designs/admin-profile.css">
 </head>
 <body>
+    <!-- Navbar for screens below 768px -->
+    <div class="mobile-navbar" id="mobile-navbar">
+        <div class="mobile-logo">
+            <img src="/images/BUZZ-Black.png" alt="Buzz Collective Logo">
+        </div>
+        <i class='bx bx-menu' id="menu-icon"></i>
+    </div>
+    
+    <aside class="sidebar" id="sidebar">
+        <i class='bx bx-x' id="close-sidebar" style="display: none;"></i> <!-- Add this line for the close button -->
+        <div class="logo">
+            <a href="/admin-home.php">
+                <img src="/images/BUZZ-White.png" alt="Buzz Collective Logo">
+            </a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="/adminprofile.php">Admin Profile</a></li>
+                <li><a href="/dashboard.php">Dashboard</a></li>
+                <li><a href="/admin-appointment.php">Appointment Bookings</a></li>
+                <li><a href="/admin-barber.php">Barbers' Schedule</a></li>
+                <li><a href="/services.php">Services</a><span class="notification-dot"></span></li>
+                <li>
+                    <a href="/admin-aboutus.php">About Us</a>
+                    <ul class="htmlCss-sub-menu sub-menu">
+                                <li><a href="/aboutus.php">Barbers</a></li>
+                                <li><a href="/aboutushiring.php">Hiring</a></li>
+                            </ul>
+                </li>
+                <li><a href="/news.php">News</a></li>
+                <li><a href="/admin-branches.php">Branches</a></li>
+            </ul>
+        </nav>
+    </aside>
+    
     <div class="profile-container">
         <h1>Admin Profile</h1>
 
