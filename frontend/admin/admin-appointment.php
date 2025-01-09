e7e304567eff57aabf8a17cc31c74c2c28b804e8
gigi <angelaasuntogallo@gmail.com>
2025-01-09 18:32:55 +0800

    adjust

--- frontend/admin/admin-appointment.php
+++ frontend/admin/admin-appointment.php
@@ -95,7 +95,7 @@ $branches = json_decode($json_data, true);
 
                         foreach ($appointments as $appointment) {
                             $receipt_link = isset($appointment['receipt']) && !empty($appointment['receipt']) 
-                            ? "<a href='#' class='view-receipt' data-receipt='" . "/Buzz-collective/frontend/admin/proxy-image.php?path=" . urlencode($appointment['receipt']) . "'>View Receipt</a>" 
+                            ? "<a href='#' class='view-receipt' data-receipt='" . "../../../admin/proxy-image.php?path=" . urlencode($appointment['receipt']) . "'>View Receipt</a>" 
                             : 'No Receipt Uploaded';
 
                             $branch_name = 'Main Branch';
